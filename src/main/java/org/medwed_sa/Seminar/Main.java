153ee58348fb9245bb1487672a54cb84c627fd1a
medvedit <113551136+medvedit@users.noreply.github.com>
2023-10-19 20:41:34 +0300

    Создание пакета для работы на лекциях.

--- src/main/java/org/medwed_sa/Seminar/Main.java
+++
@@ -1,8 +0,0 @@
-package org.medwed_sa.Seminar;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello OOP");
-    }
-}
---
+++ src/main/java/org/medwed_sa/Seminar/Seminar_1_IntroductionToOOP/EX001/Program.java
@@ -0,0 +1,7 @@
+package org.medwed_sa.Seminar.Seminar_1_IntroductionToOOP.EX001;
+
+public class Program {
+
+
+
+}
