6c2b95badb1ad3c07f82570ba66f8e15a9bbdda5
medvedit <113551136+medvedit@users.noreply.github.com>
2023-10-19 12:38:17 +0300

    Создание дерева проекта для изучения ООП.

---
+++ src/main/java/org/medwed_sa/Lessen/Main.java
@@ -0,0 +1,8 @@
+package org.medwed_sa.Lessen;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello OOP");
+    }
+}
---
+++ src/main/java/org/medwed_sa/Seminar/Main.java
@@ -0,0 +1,8 @@
+package org.medwed_sa.Seminar;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello OOP");
+    }
+}
