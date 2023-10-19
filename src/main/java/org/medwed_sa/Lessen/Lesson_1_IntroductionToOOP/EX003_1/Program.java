6a485e1b6b01fbf364d10c0ce13aaf0e31071d9e
medvedit <113551136+medvedit@users.noreply.github.com>
2023-10-19 16:50:17 +0300

    Третий пример_1. Добавление get и set, для чтения и изменения данных. Правка, соответственно и пользовательского кода.

---
+++ src/main/java/org/medwed_sa/Lessen/Lesson_1_IntroductionToOOP/EX003_1/Program.java
@@ -0,0 +1,37 @@
+package org.medwed_sa.Lessen.Lesson_1_IntroductionToOOP.EX003_1;
+
+
+/**
+ * Три разных пользовательских кода создания экземпляра класса (разделены чертой), отличаются предаваемыми данными.
+ * И в зависимости от этого используются разные конструкторы.
+ * Получили Point2D a и Point2D b с помощью разных конструкторов, передали в метод distance().
+ * У метода distance() - так-же перегрузка из двух вариантов отличающихся только разными входящими сигнатурами.
+ */
+public class Program {
+
+    static double distance(Point2D a, Point2D b) {
+        return Math.sqrt(Math.pow(a.getX() - b.getX(), 2) + Math.pow(a.getY() - b.getY(), 2));
+    }
+
+    static double distance(double x1, double x2, double y1, double y2) {
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    }
+
+    public static void main(String[] args) {
+//      ____________________
+        Point2D a = new Point2D(0.34, 4.22);
+        System.out.printf("a = %s\n", a.toString()); // печать сущности.
+//      ____________________
+        Point2D b = new Point2D();
+        b.setX(0.9854);
+        b.setY(10.033);
+        System.out.printf("b = %s\n", b); // печать сущности.
+//      ___________________
+        Point2D s = new Point2D(0.77);
+        System.out.printf("s = %s\n", s); // печать сущности.
+
+        System.out.println(distance(a, b)); // печать решения метода.
+
+    }
+
+}
